be4fd0645688945f74cf77660ea905f01a91cd54
Adrian Papari <a@onedaybeard.net>
2016-10-31 02:08:07 +0100

    PlainString AFTER

--- PlainString.bytecode.java
+++ PlainString.bytecode.java
@@ -6,11 +6,9 @@ public class net/onedaybeard/constexpr/model/PlainString {
 
   // access flags 0x19
   public final static Ljava/lang/String; s1 = "hmmhoho"
-  @Lnet/onedaybeard/constexpr/ConstExpr;() // invisible
 
   // access flags 0x19
-  public final static Ljava/lang/String; s2
-  @Lnet/onedaybeard/constexpr/ConstExpr;() // invisible
+  public final static Ljava/lang/String; s2 = "hmm-uh-hoho"
 
   // access flags 0x1
   public <init>()V
@@ -34,24 +32,4 @@ public class net/onedaybeard/constexpr/model/PlainString {
     LOCALVARIABLE ignored I L0 L1 0
     MAXSTACK = 1
     MAXLOCALS = 1
-
-  // access flags 0x8
-  static <clinit>()V
-   L0
-    LINENUMBER 7 L0
-    NEW java/lang/StringBuilder
-    DUP
-    INVOKESPECIAL java/lang/StringBuilder.<init> ()V
-    LDC "hmm"
-    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    LDC 57005
-    INVOKESTATIC net/onedaybeard/constexpr/model/PlainString.hnn (I)Ljava/lang/String;
-    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    LDC "hoho"
-    INVOKEVIRTUAL java/lang/StringBuilder.append (Ljava/lang/String;)Ljava/lang/StringBuilder;
-    INVOKEVIRTUAL java/lang/StringBuilder.toString ()Ljava/lang/String;
-    PUTSTATIC net/onedaybeard/constexpr/model/PlainString.s2 : Ljava/lang/String;
-    RETURN
-    MAXSTACK = 2
-    MAXLOCALS = 0
 }
